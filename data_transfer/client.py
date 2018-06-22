46a072b69d4be9e074f402b5fb5151619003aad4
spencerhuston <pi@raspberrypi>
2018-06-22 18:35:18 +0000

    Updated client

--- data_transfer/client.py
+++ data_transfer/client.py
@@ -2,6 +2,7 @@ import socket
 import cv2
 import numpy
 import sys
+import time
 
 if (len(sys.argv) != 3):
     print("Usage: host_ip host_port")
@@ -17,6 +18,11 @@ print("Connection with host made")
 capture = cv2.VideoCapture(0)
 print("Capturing")
 
+count = 0
+curr_count = 0
+FPS_rate = 0
+
+start_time = time.time()
 while True:
     ret, frame = capture.read()
 
@@ -27,6 +33,15 @@ while True:
 
     sock.send( str(len(stringData)).ljust(16));
     sock.send( stringData );
+    
+    count += 1
+    elapsed_time = time.time()
+    if (elapsed_time - start_time > 1):
+            start_time = time.time()
+            FPS_rate = count - curr_count
+            curr_count = count
+    
+    sys.stdout.write("\rFPS:" + str(FPS_rate))
 
 sock.close()
 cv2.destroyAllWindows() 
