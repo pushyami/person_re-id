5bbe9f7644b229c71935bf78f5e524a25e08ede0
huston2 <huston2@purdue.edu>
2018-06-29 10:36:44 -0400

    Added storage solutions

--- storage/NearPy/parse_nn_test.py
+++ storage/NearPy/parse_nn_test.py
@@ -3,8 +3,10 @@ import numpy
 import scipy
 import nearpy.utils.utils
 import re_id
+import random
+import sys
 from nearpy import Engine
-from nearpy.distances import CosineDistance
+from nearpy.distances import CosineDistance, EuclideanDistance
 from nearpy.hashes import RandomBinaryProjectionTree
 import kdtree
 
@@ -15,40 +17,50 @@ def print_results(results):
 		dist = r[2]	
 		print('  {} \t| {:.4f}'.format(data, dist))	
 
-DIM = 16
+DIM = int(sys.argv[2])
 
-POINTS = 10000 
+POINTS = int(sys.argv[3]) 
+
+rand = random.randint(1, POINTS)
 
 rbpt = RandomBinaryProjectionTree('rbpt', 64, 3)
 
 engine = Engine(DIM, lshashes=[rbpt], distance=CosineDistance())
 
-v_list = []
+with open(str(sys.argv[1])) as f:
+	content = f.readlines()
+with open(str("test_nn.txt")) as f:
+	content2 = f.readlines()
+rand_frame = None
+rand_v1 = None
+rand_v2 = None
+rand_v3 = None
 
-matrix = numpy.zeros((POINTS, DIM))
-for i in xrange(POINTS):
-	v = numpy.random.randn(DIM)
-	cam = "Cam"
-	frame = "Cam1_" + str(i)
-	new_id = re_id.Re_id(frame, cam, v)
-	matrix[i, :] = nearpy.utils.utils.unitvec(v)
-	engine.store_vector(v, new_id)
-			
-	v_list.append(v)
+count = 0
+for i in content:
+	fv = i.split()
+	frame_name = str(count)
+	count += 1
+	v = numpy.array(fv)
+	v_float = v.astype(numpy.float)
+	new_id = re_id.Re_id(frame_name, "test", v)
+	engine.store_vector(v_float, new_id)
+	
 	
 start = time.time()
 
-query = numpy.random.randn(DIM)
+fv2 = content2[0].split()
+v2 = numpy.array(fv2)
+v2_float = v2.astype(numpy.float)
 
-results = engine.neighbours(query)		
+results = engine.neighbours(v2_float)
 
 end = time.time()
-
-# print_results(results)
 data = results[0][1]
 dist = results[0][2]
 
 print("Data: " + str(data.frame_name) + ", " + str(data.camera_name) + ", " + str(data.id_time) + ", " + str(data.bucket_key))
 print("Distance: " + str(dist))		
-
+print(str(data.feature_vector))
 print('Time to find: ' + str(end - start))
+
