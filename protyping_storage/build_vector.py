3a2b858c091238274df964e9014e9de180f4c54a
Amol Moses Jha <amolmoses@gmail.com>
2018-07-23 16:48:13 -0400

    Updating files

--- protyping_storage/build_vector.py
+++ protyping_storage/build_vector.py
@@ -4,7 +4,6 @@ from nearpy.distances import CosineDistance
 from nearpy.filters import NearestFilter
 from nearpy.storage import MemoryStorage
 import numpy
-import time
 
 dimension = 1024
 
@@ -20,25 +19,31 @@ i = 0
 
 query = numpy.zeros(dimension)
 
-f = open('input_vectors', 'r')
+f = open('fvec_crop.txt', 'r')
 # Opening, reading from the file::
 for next_read_line in f:
     next_read_line = next_read_line.rstrip()
 
-    split_arr = next_read_line.split(",")
+    split_arr = next_read_line.split(" ")
     split_arr = list(map(float, split_arr))
 
     vector = numpy.asarray(split_arr)
     
     if(i == 10):
         query = vector
+        print (query)
     else:
-        engine.store_vector(vector, tuple(vector))
+        vec_data = numpy.append(vector, i)
+        engine.store_vector(vector, tuple(vec_data))
 
     i += 1
 
 # Get nearest neighbors:
 N = engine.neighbours(query)
 
+# Number of nearest neighbors:
+print(len(N))
+
 for x in N:
-    print (x[1])
+    #Printing the id of the vector here as needed:
+    print (x[1][dimension])
