d7cfc08a6e8d16169f856517e6b62031e2b0ab72
zhikaivinsontan <tan170@purdue.edu>
2018-07-23 15:29:20 -0400

    Add files via upload
    calculates similarly between 2 feature vectors
    input are two text files to compare feature vectors of each line

---
+++ Feature_Database/similarity_metric.py
@@ -0,0 +1,35 @@
+import math
+from math import sqrt
+import random
+
+from scipy import spatial
+
+def getVector(featurevector):
+	
+	body = featurevector.split()
+
+	result = []
+
+	for i in body:
+		result.append(float(i))
+
+	print (result)
+
+	return result
+
+
+num1= open("first.txt","r")
+num2= open("second.txt","r")
+
+lines1 = num1.readlines()
+lines2 = num2.readlines()
+
+
+
+for i in range(len(lines1)):
+	print (lines1[i],lines2[i])
+	first = getVector(lines1[i])
+	second = getVector(lines2[i])
+
+	print (1 - spatial.distance.cosine(first, second))
+
