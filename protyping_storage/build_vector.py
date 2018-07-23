f95d759b96f04369f0239ff3016c0c52dad7e1cc
Amol Moses Jha <amolmoses@gmail.com>
2018-07-23 16:22:17 -0400

    Working on the prototype for the storage

---
+++ protyping_storage/build_vector.py
@@ -0,0 +1,47 @@
+from nearpy import Engine
+from nearpy.hashes import RandomBinaryProjections
+from nearpy.distances import CosineDistance
+from nearpy.filters import NearestFilter
+from nearpy.storage import MemoryStorage
+import numpy
+import time
+
+dimension = 1024
+
+rbp = RandomBinaryProjections('rbp', 10)
+
+engine = Engine(dimension,
+                lshashes=[rbp],
+                distance=CosineDistance(),
+                vector_filters=[NearestFilter(10)],
+                storage = MemoryStorage())
+
+i = 0
+
+query = numpy.zeros(dimension)
+
+f = open('input_vectors', 'r')
+# Opening, reading from the file::
+for next_read_line in f:
+    next_read_line = next_read_line.rstrip()
+
+    split_arr = next_read_line.split(",")
+    split_arr = list(map(float, split_arr))
+
+    vector = numpy.asarray(split_arr)
+    
+    if(i == 10):
+        query = vector
+        print (query)
+    else:
+        engine.store_vector(vector, tuple(vector))
+
+    i += 1
+
+# Get nearest neighbors:
+N = engine.neighbours(query)
+
+print(len(N))
+
+for x in N:
+    print (x[1])
